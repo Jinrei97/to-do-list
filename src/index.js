f5a2a6a4247ea2d0110c7b9ad860e5aea5844550
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-07-12 15:19:25 +0200

    Created classes:  - ProjectList: contains the array with every     project for the user;  - Project: class of a single project;  - Task: single task of a project.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import * as css from "./index.css";
+
+class ProjectList {
+    constructor() {
+        this.project_list = [];
+    }
+    addProject(title, dueDate, description) {
+        this.project_list.push(new Project(title, dueDate, description));
+        console.log(list.project_list);
+    }
+}
+
+class Project {
+    constructor(title, dueDate, description) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.tasks = [];
+    }
+    addTask(title, dueDate, description, important) {
+        this.tasks.push(new Task(title, dueDate, description, important));
+        console.log(this.tasks);
+    }
+}
+
+class Task {
+    constructor(title, dueDate, description, important) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.important = important;
+    }    
+}
+
+const list = new ProjectList();
+
+list.addProject("Test", "12/05/2025", "prova");
+list.project_list[0].addTask("test_task", "12/04/2025", "prova Task", false);
+list.project_list[0].addTask("test_task", "12/04/2025", "prova Task", false);
+list.project_list[0].addTask("test_task", "12/04/2025", "prova Task", false);
